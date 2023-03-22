edc27ba539ea272ec748cb753061505f54d14447
lucasbmiotto <lucas.b.miotto@hotmail.com>
2023-03-22 19:13:14 -0300

    Js da página com o texto

---
+++ Modelo - Background full image/script.js
@@ -0,0 +1,9 @@
+// Definindo as palavras usadas
+var typed = new typed("span",{
+
+    // Para definir as palavras, use o seguinte modelo: strings:["Palavra 1", "Palavra 2"],
+    strings:["Landing Page Model"],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
